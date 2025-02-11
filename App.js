9070945ac638b300d4725a16b4a9b8ac11aa3984
Vicky Murugan <vickymurugan@Vickys-MacBook-Air.local>
2025-02-12 00:37:18 +0530

    Made change in node modules

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-const heading=React.createElement("h1",{id:"hello",style:{fontSize:"18vh"}},"Hello World from React")
+const heading=React.createElement("h1",{id:"hello",style:{fontSize:"10vh"}},"Hello World from React")
 
 const root=ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading)
